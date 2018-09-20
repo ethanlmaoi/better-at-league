be10cb353d890382369b2f1ac92299f04ad6d242
Ethan Liao <marineethan@gmail.com>
2018-09-20 22:11:31 +0000

    fixed requests and added delay to render data

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get("/results", function(req, res) {
     request("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + queryPlayer + "?api_key=RGAPI-5a515c06-93d3-4592-b32b-edffd9304d63", function(error, response, body) {
        if (!error && response.statusCode == 200) {
            dataPlayer = JSON.parse(body);
+           console.log(dataPlayer);
        }
     });
     
@@ -25,10 +26,15 @@ app.get("/results", function(req, res) {
     request("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + queryOpponent + "?api_key=RGAPI-5a515c06-93d3-4592-b32b-edffd9304d63", function(error, response, body) {
        if (!error && response.statusCode == 200) {
            dataOpponent = JSON.parse(body);
-           res.render("results", {dataPlayer: dataPlayer, dataOpponent: dataOpponent});
+           console.log(dataOpponent);
        }
     });
     
+    var delayInMilliseconds = 1000; //1 second
+    
+    setTimeout(function() {
+        res.render("results", {dataPlayer: dataPlayer, dataOpponent: dataOpponent});
+    }, delayInMilliseconds);
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
