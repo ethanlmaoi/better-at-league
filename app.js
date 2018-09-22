61914d58c526b05c95c54c29a33f8c972b5ffb88
Ethan Liao <marineethan@gmail.com>
2018-09-22 10:10:28 +0000

    added player score inits

--- app.js
+++ app.js
@@ -18,21 +18,21 @@ app.get("/results", function(req, res) {
     var queryOpponent = req.query.searchOpponent;
     
     // sends request to receive PLAYER summoner ID, name, profileIconId, summonerLevel
-    request("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + queryPlayer + "?api_key=RGAPI-4d505f97-d59e-4c60-af6a-3d907868a699", function(error, response, body) {
+    request("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + queryPlayer + "?api_key=RGAPI-7c362880-dcc3-49df-8ae8-a7b4f470ba54", function(error, response, body) {
        if (!error && response.statusCode == 200) {
            dataPlayer = JSON.parse(body);
            var queryAccountID = dataPlayer["accountId"];
            console.log(queryAccountID); // should print PLAYER'S accountId
            
            // sends request using player's accountId to receive matchId or gameId
-           request("https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + queryAccountID + "?api_key=RGAPI-4d505f97-d59e-4c60-af6a-3d907868a699", function(error, response, body) {
+           request("https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + queryAccountID + "?api_key=RGAPI-7c362880-dcc3-49df-8ae8-a7b4f470ba54", function(error, response, body) {
                 if (!error && response.statusCode == 200) {
                     dataMatchList = JSON.parse(body);
                     matchID = dataMatchList["matches"][0]["gameId"];
                     console.log(matchID); // should print PLAYER'S matchId
                     
                     // sends request using matchId to receive matchData
-                    request("https://na1.api.riotgames.com/lol/match/v3/matches/" + matchID + "?api_key=RGAPI-4d505f97-d59e-4c60-af6a-3d907868a699", function(error, response, body) {
+                    request("https://na1.api.riotgames.com/lol/match/v3/matches/" + matchID + "?api_key=RGAPI-7c362880-dcc3-49df-8ae8-a7b4f470ba54", function(error, response, body) {
                         if (!error && response.statusCode == 200) {
                             dataMatch = JSON.parse(body);
                             // console.log(dataMatch);
@@ -44,14 +44,14 @@ app.get("/results", function(req, res) {
     });
     
     // sends request to receive OPPONENT summoner ID, name, profileIconId, summonerLevel
-    request("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + queryOpponent + "?api_key=RGAPI-4d505f97-d59e-4c60-af6a-3d907868a699", function(error, response, body) {
+    request("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + queryOpponent + "?api_key=RGAPI-7c362880-dcc3-49df-8ae8-a7b4f470ba54", function(error, response, body) {
        if (!error && response.statusCode == 200) {
            dataOpponent = JSON.parse(body);
            console.log(dataOpponent);
        }
     });
     
-    var delayInMilliseconds = 3000; // renders all of the data
+    var delayInMilliseconds = 5000; // renders all of the data
     
     // renders results page with all data
     setTimeout(function() {
